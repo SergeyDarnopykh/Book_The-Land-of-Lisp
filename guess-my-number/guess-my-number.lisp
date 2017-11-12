4d99df21cdb89f95e9be152c91345894963426c1
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2017-11-12 23:24:56 +0300

    Add guess-my-number game

---
+++ guess-my-number/guess-my-number.lisp
@@ -0,0 +1,18 @@
+(defparameter *max* 100)
+(defparameter *min* 1)
+
+(defun guess-my-number ()	
+	(ash ( + *max* *min*) -1))
+
+(defun smaller ()
+	(setf *max* (1- (guess-my-number))) 
+	(guess-my-number))
+
+(defun bigger ()
+	(setf *min* (1+ (guess-my-number))) 
+	(guess-my-number))
+
+(defun start-over ()
+	(setf *min* 1)
+	(setf *max* 100)
+	(guess-my-number))
